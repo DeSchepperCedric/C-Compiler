fe1817300659af6184de240235a152c7f9a72e1b
kasperengelen <kasper.engelen@student.uantwerpen.be>
2019-04-25 22:15:17 +0200

    removed error handling

--- run_compiler.py
+++ run_compiler.py
@@ -58,8 +58,8 @@ def main(argv):
         run_compiler(source_file_path = argv[1], output_name=output_name)
     except CompilerException as e:
         Logger.error("Compiler was terminated due to errors in the specified C source file.")
-    except Exception as e:
-        Logger.error("Unexpected error of type '{}': {}".format(type(e), str(e)))
+    #except Exception as e:
+    #    Logger.error("Unexpected error of type '{}': {}".format(type(e), str(e)))
 
 if __name__ == "__main__":
     main(sys.argv)
