4140132f188194522d5a6f108f4028dd5b9882eb
kasperengelen <kasper.engelen@student.uantwerpen.be>
2019-04-28 02:12:14 +0200

    printf scanf testsé

--- test_files/LLVM_printf_scanf.c
+++ test_files/LLVM_printf_scanf.c
@@ -6,7 +6,20 @@
 
 int main(int argc, char** argv)
 {
-	printf("ok\n");
+	int received_int = 0;
+	printf("Enter integer:\n");
+	scanf("%d", &received_int);
+	printf("Received int: %d\n", received_int);
+
+	float recieved_float = 0.0;
+	printf("Enter float:\n");
+	scanf("%f", &recieved_float);
+	printf("Received float: %f\n", recieved_float);
+
+	char str[255];
+	printf("Enter string:\n");
+	scanf("%s", &str);
+	printf("Received string: '%s'\n", str);
 
 	return 0;
 }
