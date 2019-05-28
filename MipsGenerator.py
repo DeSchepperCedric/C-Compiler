a3122092b476485cd2d062dbe0764e1b2f8a5911
kasperengelen <kasper.engelen@student.uantwerpen.be>
2019-05-29 00:03:25 +0200

    fixed variable in addrExpr; added scanf

--- MipsGenerator.py
+++ MipsGenerator.py
@@ -83,6 +83,8 @@ class MipsGenerator:
             # manual printf processing is easier
             if node.getFunctionID().getIdentifierName() == "printf":
                 return self.printfExpr(node)
+            elif node.getFunctionID().getIdentifierName() == "scanf":
+                return self.scanfExpr(node)
             else:
                 return self.funcCallExpr(node)
 
@@ -959,7 +961,54 @@ class MipsGenerator:
         :param node: The FuncCallExpr object that represents the call to scanf()
         :return: A pair (code, -1) with code
         """
-        pass
+
+        code = ""
+
+        # get args
+        args = node.getArguments()
+
+        if len(args) != 2:
+            raise Exception("Invalid number of arguments for scanf. Expected 2, got {}.".format(len(args)))
+        
+        formatter_expr = args[0]
+        target_expr    = args[1]
+
+        if not isinstance(formatter_expr, StringConstantExpr):
+            raise Exception("First argument to scanf must be string constant.")
+
+        formatter_str = formatter_expr.getStrValue()
+
+        target_code, target_addr_reg = self.astNodeToMIPS(target_expr)
+        # target_addr_reg contains the address where the value needs to be stored.
+
+        code += target_code
+
+        if formatter_str == "%d":
+            code += "li $v0, 5\n"
+            code += "syscall\n"
+            code += self.storeRegister("$v0", target_addr_reg, 0, is_float=False)
+        elif formatter_str == "%f":
+            code += "li $v0, 6\n"
+            code += "syscall\n"
+            code += self.storeRegister("$f0", target_addr_reg, 0, is_float=True)
+        elif formatter_str == "%s":
+            code += "li $v0, 8\n"
+
+            # $a0 contains the address of the string.
+            code += "move $a0, {}\n".format(target_addr_reg)
+            code += "li $a1, 256\n"
+
+            code += "syscall\n"
+        elif formatter_str == "%c":
+            code += "li $v0, 12\n"
+            code += "syscall\n"
+            code += self.storeRegister("$v0", target_addr_reg, 0, is_float=False)
+        else:
+            raise Exception("Invalid formatter code for scanf: '{}'".format(formatter_str))
+
+        return code, -1
+
+
 
     def split_formatted_string(self, string):
         """
@@ -1172,7 +1221,7 @@ class MipsGenerator:
 
             # retrieve information about the variable
             varname = target.getIdentifierName()
-            vartype, varscope = node.getSymbolTable().lookup(identifier)
+            vartype, varscope = node.getSymbolTable().lookup(varname)
             full_id = varscope + "." + varname
 
             if not full_id in self.var_offset_dict:
